21f0c83443b334afe8a3575d9691086e7d0768a5
garret <garretleotta@users.noreply.github.com>
2024-05-15 13:17:44 -0400

    test push notification

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 Push Notification microservice
 subcribes to a rabbitMQ broker, reads messages from there and sends them off to APN (add the android one later)
 
+doesn't deal in payload business logic, all this does is take messages from the queue and forward them to the appropriate notification service
+
+
 TODO: the python listener sometimes starts before the rabbitmq server is up, crashing
---
+++ weps/test.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+import os
+import time
+import asyncio
+
+import httpx
+import jwt
+
+#only for dev environment
+if not os.environ.get('APNS_KEYID'):
+    from dotenv import load_dotenv
+    load_dotenv()
+
+key_id = os.environ.get('APNS_KEYID', 'secret')
+team_id = os.environ.get('APPLE_TEAMID', 'secret')
+with open(os.path.join('.', f"secrets/AuthKey_{key_id}.p8"), 'r') as f:
+    secret = f.read()
+
+my_device = os.environ.get('TEST_DEVICETOKEN', 'secret')
+
+payload = {"aps":{"alert":{"title":"Notification Title","subtitle":"New Message","body":"Message Content"}},"navigation":"chat:1"}
+topic = os.environ.get('APNS_TOPIC', 'secret')
+
+DEV_SERV = 'https://api.sandbox.push.apple.com'
+LIVE_SERV = 'https://api.push.apple.com'
+SERV = LIVE_SERV
+
+def create_token():
+    # TODO: iat only valid for 1 hour, method to check expiry
+    issued_at = time.time()
+    tok_dict = {
+        'iss': team_id,
+        'iat': issued_at,
+    }
+    headers = {
+        'alg': 'ES256',
+        'kid': key_id,
+    }
+    return jwt.encode(tok_dict, secret, algorithm='ES256', headers=headers)
+
+async def send_noti(client, token):
+    headers = {}
+    headers['authorization'] = f"bearer {token}"
+    headers['apns-push-type'] = 'alert'
+    # TODO: apns-expiration
+    headers['apns-priority'] = '5'
+    headers['apns-topic'] = topic
+    # TODO: for chats, this is "chat{chatId}"
+    # headers['apns-collapse-id'] = 4
+
+    url = f"{SERV}/3/device/{my_device}"
+    r = await client.post(url, headers=headers, json=payload)
+    print(r.http_version)
+    print(r.status_code)
+    print(r.content)
+
+async def main():
+    client = httpx.AsyncClient(http2=True)
+    token = create_token()
+    await send_noti(client, token)
+    
+
+if __name__ == '__main__':
+    asyncio.run(main())
